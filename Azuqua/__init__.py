a170355e0275b035c9b4e543e4d0b343d0d4edbd
Holden Stegman <stegmanh@uw.edu>
2017-07-26 22:24:12 -0700

    Add initial support for v2API

--- Azuqua/__init__.py
+++ Azuqua/__init__.py
@@ -1,91 +1,86 @@
-
 import json
 import requests
 import hmac
 import datetime
 import copy
 import hashlib
-import urllib
+import urllib.parse as urllib
+import os
 
 class Azuqua(object):
+    HTTP_OPTIONS = {
+        "protocol": "https",
+        "host": "alphaapi.azuqua.com",
+        "port": "443"
+    }
 
-	ROUTES = {
-		"list": {
-			"path": "/account/flos",
-			"method": "get"
-		},
-		"invoke": {
-			"path": "/flo/:id/invoke",
-			"method": "post"
-		}
-	}
+    def __init__(self, accessKey, accessSecret):
+        self.accessKey = accessKey
+        self.accessSecret = accessSecret
 
-	HTTP_OPTIONS = {
-		"host": "https://api.azuqua.com",
-		"headers": {
-			"Content-Type": "application/json",
-			"Accept": "*/*"
-		}
-	}
- 
-	def __init__(self, accessKey, accessSecret):
-		self.accessKey = accessKey
-		self.accessSecret = accessSecret
-		self.floCache = None
+        curr_dir = os.path.dirname(__file__)
+        with open(os.path.join(curr_dir, "../static/routes.json")) as route_file:
+            routes = json.load(route_file)
+            for _, group in routes.items():
+                for routeName, route in group.items():
+                    def outer():
+                        method = route["methods"].upper()
+                        path = route["path"]
+                        urlParams = list(filter(lambda s: s.startswith(":"), path.split("/")))
+                        def inner(*args):
+                            new_path = path
+                            for (idx, param) in enumerate(urlParams):
+                                new_path = new_path.replace(param, str(args[idx]))
+                            data = {}
+                            if method == 'POST' or method == 'PUT' and len(args) > len(urlParams):
+                                last_argument = args[len(args) - 1]
+                                if isinstance(last_argument, dict):
+                                    data = last_argument
+                            return self.request(new_path, method, data)
+                        return inner
+                    setattr(self.__class__, routeName, staticmethod(outer()))
 
-	def sign_data(self, secret, data, verb, path, timestamp):
-		if isinstance(data, dict):
-			data = str(json.dumps(data, separators=(',', ':')))
-		meta = str(":".join([verb.lower(), path, timestamp]))
-		return hmac.new(secret, meta + data, hashlib.sha256).hexdigest()
+    def account(self):
+        return { 
+            "accessKey": self.accessKey,
+            "accessSecret": self.accessSecret
+        }
 
-	def make_request(self, path, verb, data):
-		if data is None or len(data) < 1:
-			data = ""
-		headers = copy.copy(Azuqua.HTTP_OPTIONS["headers"])
-		timestamp = datetime.datetime.utcnow().isoformat()
-		headers["x-api-accessKey"] = self.accessKey
-		headers["x-api-timestamp"] = timestamp
-		headers["x-api-hash"] = self.sign_data(self.accessSecret, data, verb, path, timestamp)
-		if verb == "get":
-			if isinstance(data, dict):
-				path += urllib.quote_plus(data)
-			resp = requests.get(Azuqua.HTTP_OPTIONS["host"] + path, headers=headers)
-			return resp.json() or resp.text
-		elif verb == "post":
-			resp = requests.post(Azuqua.HTTP_OPTIONS["host"] + path, data=json.dumps(data, separators=(',', ':')), headers=headers)
-			return resp.json() or resp.text
+    def request(self, path, verb, data):
+        if (verb == "GET" or verb == "DELETE") and bool(data):
+            querystring = urllib.urlencode(data)
+            path = path + "?" + querystring
+            data = {}
 
+        headers = {
+            "Content-Type": "application/json"
+        }
+        timestamp = datetime.datetime.utcnow().isoformat()
+        headers["x-api-accessKey"] = self.accessKey
+        headers["x-api-timestamp"] = timestamp
+        headers["x-api-hash"] = self.sign_data(self.accessSecret, path, verb, data, timestamp)
 
-	def account(self):
-		return { 
-			"accessKey": self.accessKey,
-			"accessSecret": self.accessSecret
-		}
+        http_options = Azuqua.HTTP_OPTIONS
+        url = http_options["protocol"] + "://" + http_options["host"] + ":" + http_options["port"] + path
+        if verb == "GET":
+            resp = requests.get(url, headers=headers)
+            return resp.json() or resp.text
+        elif verb == "DELETE":
+            resp = requests.delete(url, headers=headers)
+            return resp.json() or resp.text
+        elif verb == "PUT":
+            data = str(json.dumps(data, separators=(',', ':')))
+            resp = requests.put(url, data=data, headers=headers)
+            return resp.json() or resp.text
+        else:
+            data = str(json.dumps(data, separators=(',', ':')))
+            resp = requests.post(url, data=data, headers=headers)
+            return resp.json() or resp.text
 
-	def create_flo(self, name, alias):
-		return Azuqua.Flo(self, name, alias)
-
-	def flos(self, refresh=False):
-		if refresh or self.floCache is None:
-			resp = self.make_request(Azuqua.ROUTES["list"]["path"], Azuqua.ROUTES["list"]["method"], None)
-			if isinstance(resp, dict) and "error" in resp:
-				raise Exception(resp["error"] or "Error listing flos")
-			else:
-				self.floCache = map(lambda curr: self.create_flo(curr["name"], curr["alias"]), resp)
-		return self.floCache
-
-	class Flo(object):
-
-		def __init__(self, wrapper, name, alias):
-			self.name = name
-			self.alias = alias
-			self.wrapper = wrapper
-
-		def invoke(self, data):
-			path = Azuqua.ROUTES["invoke"]["path"].replace(":id", self.alias)
-			resp = self.wrapper.make_request(path, Azuqua.ROUTES["invoke"]["method"], data)
-			if isinstance(resp, dict) and "error" in resp:
-				raise Exception(resp["error"] or "Error invoking flo")
-			else:
-				return resp
+    def sign_data(self, secret, path, verb, data, timestamp):
+        if not bool(data):
+            data = ""
+        else:
+            data = str(json.dumps(data, separators=(',', ':')))
+        meta = str(":".join([verb.lower(), path, timestamp])) + data
+        return hmac.new(secret.encode(), meta.encode(), hashlib.sha256).hexdigest()
