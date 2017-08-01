70586e05ed78d3b21d93c22f0c6a34b211a87373
Holden Stegman <stegmanh@uw.edu>
2017-08-01 15:20:14 -0700

    Fix setup script

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 
 from distutils.core import setup
 
-setup(name='Azuqua',
-	version='0.1.0',
-	description='The Azuqua API python library',
-	author='Alec Embke',
-	author_email='alec@azuqua.com',
-	url='http://developer.azuqua.com',
-	packages=['Azuqua']
+setup(
+    name='Azuqua',
+    version='0.1.0',
+    description='The Azuqua API python library',
+    author='Alec Embke',
+    author_email='alec@azuqua.com',
+    url='http://developer.azuqua.com',
+    packages=['Azuqua'],
+    package_data={'Azuqua': ['static/*.json']}
 )
